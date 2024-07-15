a69c400387989d751bb320021a9a83f780ef487f
Emmanuel687 <koechemmanuel2002@gmail.com>
2024-07-15 23:21:14 +0300

    Added Route Params

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -1,9 +1,15 @@
 <template>
 	<div class="posts">
 		<h1>This is a Post Page</h1>
+		<p>Display the content of post with ID of {{ $route.params.id }}</p>
+		<p>
+			<RouterLink to="/posts">&lt; Back </RouterLink>
+		</p>
+
 	</div>
 </template>
 
 <script setup>
-	import { vAutofocus } from "../directives/vAutoFocus";
+import { RouterLink } from "vue-router";
+import { vAutofocus } from "../directives/vAutoFocus";
 </script>
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,10 +1,32 @@
 <template>
 	<div class="posts">
 		<h1>Posts</h1>
+		<uL>
+			<li>
+				<RouterLink to="/postDetail/id1">Post 1</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/postDetail/id2">Post 2</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/postDetail/id3">Post 3</RouterLink>
+			</li>
+		</uL>
 		<textarea v-autofocus></textarea>
+
+
 	</div>
 </template>
 
 <script setup>
-	import { vAutofocus } from "../directives/vAutoFocus";
+import { RouterLink } from "vue-router";
+import { vAutofocus } from "../directives/vAutoFocus";
 </script>
+
+
+<style scoped>
+ul {
+
+	margin-bottom: 30px;
+}
+</style>
