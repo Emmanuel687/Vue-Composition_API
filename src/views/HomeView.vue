e5831bf0e3e8218f162a937a11f6ac3a90d29ed4
Emmanuel687 <koechemmanuel2002@gmail.com>
2024-07-26 22:09:59 +0300

    Created Templated Ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    
+    <h1 ref="appTitleRef">Vue3</h1>
+    <h3>Vue4</h3>
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
       <span class="counter">{{ counter }}</span>
@@ -11,9 +12,14 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
+const appTitleRef = ref(null)
 const counter = ref(0)
+const counterData = [{
+  count: 0,
+  title: "My Counter"
+}]
 
 const increaseCounter = () => {
   counter.value++
@@ -22,6 +28,12 @@ const increaseCounter = () => {
 const decreaseCounter = () => {
   counter.value--
 }
+
+onMounted(() => {
+  console.log(`The app title is ${appTitleRef.value.offsetWidth}  px`)
+  // console.log(appTitleRef.value.offsetWidth)
+
+})
 </script>
 
 <!--
@@ -75,7 +87,9 @@ export default {
   text-align: center;
   padding: 20px;
 }
-.btn, .counter {
+
+.btn,
+.counter {
   font-size: 40px;
   margin: 10px;
 }
